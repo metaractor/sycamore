ee3f78a7faa38adc0c81c55b0a2d5cd7dd03ec80
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-08-26 13:09:52 +0000

    Handle rake

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -26,6 +26,10 @@ fi
 
 if [ "$1" = 'bundle' ]; then
   set -- su-exec sycamore "$@"
+elif [ "$1" = 'rake' ]; then
+  set -- su-exec sycamore bundle exec "$@"
+
+  su-exec sycamore bash -c 'bundle check || bundle install'
 elif ls /usr/local/bundle/bin | grep -q "\b$1\b"; then
   set -- su-exec sycamore bundle exec "$@"
 
